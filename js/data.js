75ccd987e6075ff1ed142eedaebcfdf48035cc8e
islom2202 <islomihsan@gmail.com>
2024-04-12 16:44:06 +0500

    added layouts

---
+++ js/data.js
@@ -0,0 +1,11 @@
+export const data = {
+  main: {
+    name: 'Welcome to the <br><span>Frontend Quiz!</span>',
+    buttons: [
+      {
+        image: "",
+        name: "html"
+      }
+    ]
+  }
+}
---
+++ js/main.js
@@ -0,0 +1,3 @@
+import { switchTheme } from "./switcher.js";
+import { data } from "./data.js";
+document.querySelector(".js-theme-toggler").onclick = () => switchTheme()
