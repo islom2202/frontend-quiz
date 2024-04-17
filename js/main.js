0bc0d6c6e19521975efde710e93e40ec2a6ed859
islom2202 <islomihsan@gmail.com>
2024-04-17 11:44:04 +0500

    added menu stylings

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 import { switchTheme } from "./switcher.js";
-import { data } from "./data.js";
 document.querySelector(".js-theme-toggler").onclick = () => switchTheme()
+
+/*///////////
+ General steps:
+ 1. Save data
+ 2. Generate HTML
+ 3. Make it inteactive
+*////////////
+
+// console.log(((1/12)*100).toFixed(0));
